070909eeaa7e93a50f7880430ef5b7f7b749b295
ella <elalek@gmail.com>
2025-06-16 20:31:55 +0200

    uprade

--- mysql_examples/create_connection.py
+++ mysql_examples/create_connection.py
@@ -18,3 +18,13 @@ if conn:
     cursor.close()
 
     conn.close()
+
+#jak to spustit na macu:
+        #Open the integrated terminal in VS Code.
+        # Run python3 -m venv .venv to create a virtual environment.
+        # Activate it with source .venv/bin/activate.
+        # Run pip install PyMySQL inside the activated environment.
+# zadat do terminalu
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip install PyMySQL
